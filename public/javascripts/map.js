8d516348ea32f957978aa1781d2c7b18db3899dc
Santiago Hidalgo <santiago.hidalgo.ext@holcim.com>
2023-04-23 17:58:56 -0500

    🔥 Added specific geolocalization

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,6 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([6.2476, -75.5658], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([6.2476, -75.5658]).addTo(map)
+    .bindPopup('My bici')
+    .openPopup();
